f906809a3a6b127c09d1697a0f9009dbebdbd61a
Ryan Stone <rstone@FreeBSD.org>
2018-03-05 21:18:37 -0500

    Ensure that src()/dst() are available in TcpHeader.h

--- include/pktgen/TcpHeader.h
+++ include/pktgen/TcpHeader.h
@@ -39,6 +39,7 @@ extern "C" {
 
 #include "pktgen/EncapsulatableHeader.h"
 #include "pktgen/Layer.h"
+#include "pktgen/L2Fields.h"
 #include "pktgen/L3Fields.h"
 #include "pktgen/L4Fields.h"
 #include "pktgen/PayloadLength.h"
